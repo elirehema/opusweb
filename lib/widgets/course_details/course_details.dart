1323a350ff411f0f3b1aa541dd6bb96a60465c45
=elirehema <=rocksandystone@gmail.com>
2020-06-27 17:10:05 +0300

    Updated deatails

--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -7,25 +7,23 @@ class CourseDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 600,
-      child: Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'FLUTTER WEB.\nTHE BASICS',
-              style: TextStyle(
-                  fontWeight: FontWeight.w800, fontSize: 80, height: 0.9),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
-              style: TextStyle(fontSize: 21, height: 1.7),
-            ),
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'FLUTTER WEB.\nTHE BASICS',
+            style: TextStyle(
+                fontWeight: FontWeight.w800, fontSize: 80, height: 0.9),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
+            style: TextStyle(fontSize: 21, height: 1.7),
+          ),
+        ],
       ),
     );
   }
